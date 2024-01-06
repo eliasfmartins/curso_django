d1442f3f06d4bb9503df5e355cd01eade5873d8f
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-06 10:51:28 -0300

    separando urls e views

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.http import HttpResponse
-
-# HTTP REQUEST
-def my_view(request):
-    return HttpResponse('Home')
-    # return HTTP Response
+from django.urls import path , include
+from recipes.views import home ,sobre, _contato
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
+    path('', include('recipes.urls')),
+    #basicamente incluiu essas urls na home 
+    path('recipes/', include('recipes.urls')),
+    #nesse caso incluiu essas urls dps da rota recipes
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import path
+from django.http import HttpResponse
 
+
+# HTTP REQUEST
+def home(request):
+    return HttpResponse('Home')
+    # return HTTP Response
 # Create your views here.
+def _contato(request):
+    return HttpResponse('Contato')
+
+def sobre(response):
+    return HttpResponse('Sobre')
