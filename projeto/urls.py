18c914899edda1b1fc8bbca85fff016c70d73687
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-06 10:14:58 -0300

    introducao a URL do django e Views

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# HTTP REQUEST
+def my_view(request):
+    return HttpResponse('Elias e o FOdao')
+    # return HTTP Response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
