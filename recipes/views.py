339f50591ed12cbc7e0b4ee1bf624472584bb75b
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-06 14:43:08 -0300

    limpando views

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.urls import path
-from django.http import HttpResponse
 
 
 # HTTP REQUEST
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name':'Elias the Fodao'
     } )
     # return HTTP Response
