973f8312508d40a062e476abbe8ea6240d2c7fa0
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-08 20:45:25 -0300

    atualizando urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from recipes.views import home 
+from . import  views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe ),
 ]
+# usa se <nome param> para pegar o valor dinamico
+#dentro do < passar o tipo de propriedade vai ser aceito como parametro no meu caso <int:>
+# tem varios tipos na documentação
