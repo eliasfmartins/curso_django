7b225d2058b5b4bf5aafbd20cd25beb8ee1d87c6
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-10 09:24:13 -0300

    change files  for template

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 # HTTP REQUEST
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name':'Elias the Fodao'
+        'recipes':[make_recipe() for _ in range(10)]
     } )
     
 # return HTTP Response
 # Create your views here.
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name':'Elias the Fodao'
+        'recipe': make_recipe(),
     } )
 
