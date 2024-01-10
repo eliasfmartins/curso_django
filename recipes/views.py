3f062ead0d240113236aae615bd2c4aa4f9e0e90
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-10 12:22:29 -0300

    att viwer

--- recipes/views.py
+++ recipes/views.py
@@ -12,5 +12,6 @@ def home(request):
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page':True,
     } )
 
