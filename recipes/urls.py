bd5866951dc7347dcf7e055be69b10a129158182
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-11 12:10:33 -0300

    links dinamicos

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe ),
+    path('', views.home, name='recipes-home') ,
+    path('recipes/<int:id>/', views.recipe, name='recipes-recipe' ),
 ]
 # usa se <nome param> para pegar o valor dinamico
 #dentro do < passar o tipo de propriedade vai ser aceito como parametro no meu caso <int:>
