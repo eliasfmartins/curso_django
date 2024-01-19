70883c7e1c38c3322a2ac9bc26982f833b387961
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-19 20:40:09 -0300

    Page not found

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import  Http404
 
 # HTTP REQUEST
 def home(request):
@@ -11,9 +12,13 @@ def home(request):
     
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id,is_published=True).order_by('-id')
+    
+    if not recipes:
+        raise Http404('Not found🥹')
+    
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name}'
+        'title': f'{recipes.first().category.name} - Category | '
     } )
     
 # return HTTP Response
