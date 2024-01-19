64d7f5f8ae5ebece1e3518e77e701d139dbce3f8
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-19 20:03:56 -0300

    title dinamico

--- recipes/views.py
+++ recipes/views.py
@@ -13,6 +13,7 @@ def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id,is_published=True).order_by('-id')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name}'
     } )
     
 # return HTTP Response
