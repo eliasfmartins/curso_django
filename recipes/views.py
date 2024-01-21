130606561f034fc05da366c3056fafb9433980e9
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-21 08:27:41 -0300

    feat: exibindo html no template

--- recipes/views.py
+++ recipes/views.py
@@ -5,28 +5,34 @@ from recipes.models import Recipe
 
 # HTTP REQUEST
 def home(request):
-    recipes = Recipe.objects.filter(is_published =True).order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
-    } )
-    
+    })
+
+
 def category(request, category_id):
     # recipes = Recipe.objects.filter(category__id=category_id,is_published=True).order_by('-id')
-    
+
     # if not recipes:
     #     raise Http404('Not found🥹')
-    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id,is_published=True).order_by('-id'))
-    
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
-    } )
-    
+    })
+
 # return HTTP Response
 # Create your views here.
-def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
-        'is_detail_page':True,
-    } )
 
+
+def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id, is_published=True
+        ).order_by('-id').first()
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
