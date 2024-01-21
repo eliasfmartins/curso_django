95c19824706758f23dbc1f35e27e0b7cb6c3e605
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-21 08:39:16 -0300

    att methodo page  not found

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -29,9 +29,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id, is_published=True
-        ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
